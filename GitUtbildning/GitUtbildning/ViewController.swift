bfc5fa1d48949e4d4cedf6951de199a4c355b434
Petar Mataic <petar@petar.se>
2015-03-19 21:14:05 +0100

    Created an empty project

---
+++ GitUtbildning/GitUtbildning/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  GitUtbildning
+//
+//  Created by Petar Mataic on 19/03/15.
+//  Copyright (c) 2015 Petar Mataic. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
