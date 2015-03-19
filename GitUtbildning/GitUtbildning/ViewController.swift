a88ce1dfc1aa9c691b91f96995b8914ef2406fa5
Petar Mataic <petar@petar.se>
2015-03-19 22:34:36 +0100

    added i

--- GitUtbildning/GitUtbildning/ViewController.swift
+++ GitUtbildning/GitUtbildning/ViewController.swift
@@ -10,6 +10,8 @@ import UIKit
 
 class ViewController: UIViewController {
 
+    let i = 0
+
     @IBOutlet weak var imageView: UIImageView!
     override func viewDidLoad() {
         super.viewDidLoad()
